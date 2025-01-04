ed1f43b931466cbb8274ba3f1db44a28109ba04d
Sandhyakhade <khadesandhya95@gmail.com>
2025-01-04 22:51:29 +0530

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width-device-width",initial-scale=1.0">
+   <title>Coffee Shop Order System</title>
+   <link rel="stylesheet" href="home.css">
+ </head>   
+ <body>
+        <div class="container">
+        <header>
+            <div class="logo"><img src="cofeelogo.png"><a>Go Coffee</a></div>
+            <nav>
+                <ul class="nav-links">
+                 <li><a href="#home">Home</a></li>
+                 <li><a href="menu.php">Menu</a></li>
+                 <li><a href="orderpage.php">Order</a></li>
+                 <li><a href="adminlogin.php">Admin</a></li>
+                 <li><a href="#contact">Contact Us</a></li>
+                </ul>
+            </nav>
+        </header>   
+       <section>
+        <h1>TIME TO DISCOVER COFFEE HOUSE</h1>
+        <button type="submit"><a href="registration.php">Create Account</a></button>
+        <button type="submit"><a href="login.php">Login</a></button>
+        
+       </section>
+    </div>
+ </body>
+</html>
